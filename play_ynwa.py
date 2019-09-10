6adae89ebbc82afe131a884e93df8eb4e9737c14
hardeep-leyl <54153538+hardeep-leyl@users.noreply.github.com>
2019-09-09 21:37:28 -0400

    Update play_ynwa.py

--- play_ynwa.py
+++ play_ynwa.py
@@ -9,6 +9,7 @@ import playsound
 
 #url = "http://oit2.scps.nyu.edu/~meretzkm/python/string/romeo.txt"
 url = "https://github.com/sf19pb1-hardeep-leyl/play/blob/master/Liverpool_You_ll_Never_Walk_Alone.mp3"
+file = "/Users/kadysafar/Desktop/Liverpool_You_ll_Never_Walk_Alone.mp3"
 
 try:
     ynwa = urllib.request.urlopen(url)
@@ -16,7 +17,9 @@ except urllib.error.URLError as error:
     print(error, file = sys.stderr)
     sys.exit(1)
 
-playsound.playsound(ynwa, True)
+#Doesn't work when i call the url but works playinbg a local copy of the mp3
+#playsound.playsound(ynwa, True)
+playsound.playsound(file, True)
 
 ynwa.close()
 sys.exit(0)
