63504534d0fa565b3e1e26a46913a0550d66dd30
hardeep-leyl <54153538+hardeep-leyl@users.noreply.github.com>
2019-09-10 19:29:41 -0400

    Update play_ynwa.py

--- play_ynwa.py
+++ play_ynwa.py
@@ -1,5 +1,5 @@
 """
-play_ynwa.py.py
+play_ynwa.py
 Plays You'll Never Walk Alone
 """
 
@@ -7,8 +7,10 @@ import sys
 import urllib.request
 import playsound
 
-url = "https://github.com/sf19pb1-hardeep-leyl/play/blob/master/Liverpool_You_ll_Never_Walk_Alone.mp3"
-file = "/Users/kadysafar/Desktop/Liverpool_You_ll_Never_Walk_Alone.mp3"
+#file = "/Users/kadysafar/Desktop/Liverpool_You_ll_Never_Walk_Alone.mp3"
+#url = "https://github.com/sf19pb1-hardeep-leyl/play/blob/master/Liverpool_You_ll_Never_Walk_Alone.mp3"
+url = "http://www.fcsongs.com/uploads/audio" \
+    "/Liverpool%20FC%20-%20You%20Will%20Never%20Walk%20Alone.mp3"
 
 try:
     ynwa = urllib.request.urlopen(url)
@@ -17,8 +19,8 @@ except urllib.error.URLError as error:
     sys.exit(1)
 
 #Doesn't work when I call the url but works playing a local copy of the mp3
-#playsound.playsound(ynwa, True)
-playsound.playsound(file, True)
+#playsound.playsound(file, True)
+playsound.playsound(ynwa, True)
 
 ynwa.close()
 sys.exit(0)
